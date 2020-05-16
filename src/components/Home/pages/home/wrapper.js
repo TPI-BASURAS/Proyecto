6a3f6cb7e52581dab5c2e70f1f7f39a89ed86588
Daniel Castañeda <you@example.com>
2020-05-16 11:56:15 -0500

    First setup, missing folders

---
+++ src/components/Home/pages/home/wrapper.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from '../../../../logo.svg';
+import './wrapper.scss'
+
+function Wrapper() {
+  return (
+    <div className="wrapper-container">
+      <h1>pene</h1>
+    </div>
+  );
+}
+
+export default Wrapper;
